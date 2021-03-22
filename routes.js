bd3560d79e046a2b0d6e26e509d8d75142591263
delasalle-sio-blandin-l <delasalle-sio-blandin-l@gmail.com>
2021-03-22 18:14:16 +0100

    maj dockerfile

--- routes.js
+++ routes.js
@@ -8,8 +8,8 @@ const form = async (req, res, next) => {
 
 const validation = async (req, res, next) => {
 	const { nom, prenom, datenaissance, email, telephone, adresse } = req.body;
-	if ( !nom || !prenom|| datenaissance.length === 0 || !email || !telephone || !adresse)
-		res.status(400).sendFile(path.join(__dirname + '/vue/erreur.html'));
+	if ( !nom || !prenom || !email || !telephone || !adresse)
+		res.status(404).sendFile(path.join(__dirname + '/vue/erreur.html'));
     else {
         clients.create({
             nom: clients.nom,
@@ -19,7 +19,7 @@ const validation = async (req, res, next) => {
             telephone: clients.telephone,
             adresse: clients.adresse,
         }).then(() => {
-            res.status(201).sendFile(path.join(__dirname + '/vue/succes.html'));
+            res.status(200).sendFile(path.join(__dirname + '/vue/succes.html'));
         })
     } 
 };
