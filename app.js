a582cb5b9f08559c61fe041e229673c35a77423e
delasalle-sio-blandin-l <delasalle-sio-blandin-l@gmail.com>
2021-03-22 21:53:39 +0100

    add dockerfile and pma folder and nginx folder

--- app.js
+++ app.js
@@ -1,30 +1,3 @@
-// // CONF
-// const express = require ('express')
-// const fs = require('fs')
-// const app = express()
-
-// //DB CONNECTION 
-// try {
-//     await sequelize.authenticate();
-//     console.log('Connection has been established successfully.');
-// } catch (error) {
-//     console.error('Unable to connect to the database:', error);
-// }
-
-
-// // START PROJECT ON FORM
-// // NOT WORK WELL
-// app.get('/', (req, res) => {
-//     res.writeHead(200, {'Content-Type': 'text/html'});
-//     fs.readFile('form.html');
-// });
-
-
-// app.listen(3000, () => {
-//     console.log(`Backend is running on http://localhost:3000`)
-//   })
-
-
 const express = require('express');
 const routes = require('./routes');
 const app = express();
