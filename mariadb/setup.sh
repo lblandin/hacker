5292bb4d808a4f94159e0c6a200ecbf6c7c4824d
delasalle-sio-blandin-l <delasalle-sio-blandin-l@gmail.com>
2021-03-22 22:56:41 +0100

    change setup.sh

--- mariadb/setup.sh
+++ mariadb/setup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 export $(/.env | xargs) > /dev/null
-sed -i -e 's/MYSQL_SECU_PASSWORD/'`echo $MYSQL_SECU_PASSWORD`'/g' /setup.sql
-mysql -u root -p`echo $MYSQL_ROOT_PASSWORD` < /setup.sql
+sed -i -e 's/MYSQL_SECU_PASSWORD/'`echo $MYSQL_SECU_PASSWORD`'/g' /admin.sql
+mysql -u root -p`echo $MYSQL_ROOT_PASSWORD` < /admin.sql
 
 
 GREEN='\033[0;32m'
